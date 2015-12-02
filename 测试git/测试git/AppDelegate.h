598f146f6717d9536dcca1a32788eade5119ab7b
renhai zhou <672487421qq.com>
2015-12-02 17:31:09 +0800

    初始化

---
+++ 测试git/测试git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  测试git
+//
+//  Created by archermind on 15/12/2.
+//  Copyright © 2015年 chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
